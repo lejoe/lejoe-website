e469be6017b4ad10f44658054915a13f622a1154
lejoe <ciao@lejoe.com>
2022-01-24 11:09:29 +0100

    Fresh start

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,50 +1,50 @@
 export default {
-  // Target (https://go.nuxtjs.dev/config-target)
+  // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
-  // Global page headers (https://go.nuxtjs.dev/config-head)
+  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'lejoe-website',
+    htmlAttrs: {
+      lang: 'en'
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
   },
 
-  // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
 
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [],
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
 
-  // Auto import components (https://go.nuxtjs.dev/config-components)
+  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
     // https://go.nuxtjs.dev/stylelint
     '@nuxtjs/stylelint-module',
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
-    // Doc: https://github.com/nuxt-community/google-fonts-module
-    '@nuxtjs/google-fonts',
-    '@nuxt/postcss8',
   ],
 
-  // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [],
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+  ],
 
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {},
-
-  googleFonts: {
-    families: {
-      Inter: true,
-      Raleway: [900],
-    },
-  },
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
 }
