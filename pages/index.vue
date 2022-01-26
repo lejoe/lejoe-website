43e65e396c7aa9b6da285dee81a32b4f9ef564e7
lejoe <ciao@lejoe.com>
2022-01-26 15:35:43 +0100

    Adds first content version

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,69 @@
 <template>
+  <div>
+    <div class="font-big">
+      <h1 class="text-3xl md:text-4xl font-black">JOËL BEZ</h1>
+      <h2 class="text-xl md:text-3xl font-black">Technical project lead</h2>
+    </div>
+    <div class="pt-24 pb-24">
+      <div class="text-xl">
+        I lead and coach multidisciplinary teams to build digital products with
+        more impact.
+      </div>
+    </div>
+    <div class="pt-24 pb-40">
+      <h3 class="font-black font-big text-xl pb-5">FOCUS</h3>
+      <div class="grid grid-cols-1 md:gap-8 md:grid-cols-3 pb-32">
+        <div class="pb-5">
+          <h4 class="text-xl pb-3">Project management</h4>
+          <div class="pb-3">
+            I help aligning teams behind a common vision and creating a
+            collaborative culture.
+          </div>
+          <div>
+            I apply principles and good practices I gathered from the dozens of
+            projects and team setups I had the opportunity to take part in. Some
+            of my inspirations are lean and agile development, shape up and teal
+            organizations.
+          </div>
+        </div>
+        <div class="pb-5">
+          <h4 class="text-xl pb-3">Design driven</h4>
+          <div class="pb-3">
+            I help delivering better products by advocating for a more user
+            centered approach to development.
+          </div>
+          <div>
+            I'm passionate about optimizing the transition from concept to
+            implementation. I help developers and designers to find better ways
+            to collaborate. We develop strategies to get more efficiently to a
+            product with more value for the users.
+          </div>
+        </div>
+        <div class="pb-5">
+          <h4 class="text-xl pb-3">Development expertise</h4>
+          <div class="pb-3">
+            I help finding the sweet spot between quality and pragmatism in
+            development.
+          </div>
+          <div>
+            From ActionScript to typescript, from front end to DevOps via back
+            end, from learning to leading and coaching, I’ve had seen it all in
+            the last 20+ years in web development.
+          </div>
+        </div>
+      </div>
+      <h3 class="font-black font-big text-xl pb-5">CONTACT</h3>
+      <div class="pb-5">
+        <a href="mailto:contact@lejoe.com">contact@lejoe.com</a>
+      </div>
+    </div>
+    <div class="w-full flex justify-center">
+      <a href="https://twitter.com/lejoe" aria-label="Twitter profile">Twitter</a>
+      <a href="https://linkedin.com/in/joelbez/" aria-label="LinkedIn profile">LinkedIn</a>
+      <a href="https://github.com/lejoe" aria-label="Github profile">Github</a>
+      <a href="https://read.cv/lejoe" aria-label="CV">CV</a>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
