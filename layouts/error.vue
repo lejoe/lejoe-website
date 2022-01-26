1919b4fe81ec47b2979a099358e5139171a677f3
lejoe <ciao@lejoe.com>
2022-01-26 15:34:47 +0100

    Adds error page

---
+++ layouts/error.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <h1 v-if="error.statusCode === 404" class="text-2xl md:text-3xl font-black">Page not found</h1>
+    <h1 v-else class="text-2xl md:text-4xl font-black">An error occurred</h1>
+
+    <h2 class="text-xl md:text-2xl font-black">
+      Back to
+      <NuxtLink to="/">Home page</NuxtLink>
+    </h2>
+  </div>
+</template>
+
+<script lang="ts">
+import {
+  PropType,
+} from '@nuxtjs/composition-api'
+
+import {
+  NuxtError,
+} from '@nuxt/types'
+
+export default {
+  props: {
+    error: {
+      type: Object as PropType<NuxtError>,
+      default: null
+    }
+  },
+}
+</script>
