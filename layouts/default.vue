df5af18f9d45fdc2d7c7f8ad7eb86e7c3a0be2f2
lejoe <ciao@lejoe.com>
2022-01-26 15:34:23 +0100

    Adds a layout

---
+++ layouts/default.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="pt-12 md:pt-16 lg:pt-32 px-6 md:px-8 md:px-12 font-sans">
+    <div class="max-w-4xl mx-auto">
+      <div class="min-h-90">
+        <Nuxt />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+html {
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+a {
+  text-decoration: underline;
+}
+</style>
