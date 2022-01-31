50560db92c99a907bf6d9647231974ed41d24663
lejoe <ciao@lejoe.com>
2022-01-31 17:30:06 +0100

    Improves readability

--- components/HomeLead.vue
+++ components/HomeLead.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="font-big">
-    <h1 class="text-3xl sm:text-4xl font-black">JOËL BEZ</h1>
-    <h2 class="text-2xl sm:text-3xl font-black">Technical project lead</h2>
+  <div>
+    <h1 class="text-3xl sm:text-4xl font-black font-big">JOËL BEZ</h1>
+    <h2 class="text-2xl sm:text-3xl">Technical project lead</h2>
   </div>
 </template>
--- components/HomePurpose.vue
+++ components/HomePurpose.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="sm:text-2xl text-xl">
+  <div class="sm:text-3xl text-2xl">
     I lead and coach multidisciplinary teams to build digital products with more
     impact.
   </div>
