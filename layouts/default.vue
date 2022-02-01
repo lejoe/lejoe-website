5858956a271247e614ada0c1558425dd4a646852
lejoe <ciao@lejoe.com>
2022-02-01 15:08:01 +0100

    Removes uneeded div

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="pt-12 md:pt-24 lg:pt-32 px-6 sm:px-12 md:px-12 font-sans">
     <div class="xl:max-w-5xl max-w-4xl mx-auto overflow-hidden">
-      <div class="">
-        <main><Nuxt /></main>
-        <footer><TheFooter /></footer>
-      </div>
+      <main><Nuxt /></main>
+      <footer><TheFooter /></footer>
     </div>
   </div>
 </template>
