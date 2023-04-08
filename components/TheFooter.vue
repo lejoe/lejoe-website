7ee24dc695f25ff43c95e0f0d82e862110f4225f
lejoe <ciao@lejoe.com>
2023-04-08 20:16:25 +0200

    Replaces Twitter profile with mastodon

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,20 +1,8 @@
 <template>
   <nav class="flex items-center justify-center py-24">
-    <a
-      href="https://twitter.com/lejoe"
-      class="mx-4"
-      aria-label="Twitter profile"
-      >Twitter</a
-    >
-    <a
-      href="https://linkedin.com/in/joelbez/"
-      class="mx-4"
-      aria-label="LinkedIn profile"
-      >LinkedIn</a
-    >
-    <a href="https://github.com/lejoe" class="mx-4" aria-label="Github profile"
-      >Github</a
-    >
+    <a href="https://mastodon.social/@lejoe" class="mx-4" aria-label="Mastodon profile">Mastodon</a>
+    <a href="https://linkedin.com/in/joelbez/" class="mx-4" aria-label="LinkedIn profile">LinkedIn</a>
+    <a href="https://github.com/lejoe" class="mx-4" aria-label="Github profile">Github</a>
     <a href="https://read.cv/lejoe" class="mx-4" aria-label="CV">CV</a>
   </nav>
 </template>
