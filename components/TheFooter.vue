7b26e7103438d39e5ae596b1592a75279fe4b375
lejoe <ciao@lejoe.com>
2023-04-08 21:31:01 +0200

    Corrects code formatting

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,8 +1,20 @@
 <template>
   <nav class="flex items-center justify-center py-24">
-    <a href="https://mastodon.social/@lejoe" class="mx-4" aria-label="Mastodon profile">Mastodon</a>
-    <a href="https://linkedin.com/in/joelbez/" class="mx-4" aria-label="LinkedIn profile">LinkedIn</a>
-    <a href="https://github.com/lejoe" class="mx-4" aria-label="Github profile">Github</a>
+    <a
+      href="https://mastodon.social/@lejoe"
+      class="mx-4"
+      aria-label="Mastodon profile"
+      >Mastodon</a
+    >
+    <a
+      href="https://linkedin.com/in/joelbez/"
+      class="mx-4"
+      aria-label="LinkedIn profile"
+      >LinkedIn</a
+    >
+    <a href="https://github.com/lejoe" class="mx-4" aria-label="Github profile"
+      >Github</a
+    >
     <a href="https://read.cv/lejoe" class="mx-4" aria-label="CV">CV</a>
   </nav>
 </template>
