0a773e4226e137a15021697529305ce1f637ac39
lejoe <ciao@lejoe.com>
2023-04-08 17:54:04 +0200

    Adds console message to hint to CSS naked day

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable no-console -->
 <template>
   <div>
     <div class="pb-24">
@@ -14,5 +15,17 @@ import Vue from 'vue'
 
 export default Vue.extend({
   name: 'IndexPage',
+  mounted() {
+    console.info("Hi! Wondering why there is no styles?")
+    console.info("Well… it's…")
+    console.info(`
+   ____ ____ ____    _   _       _            _   ____
+  / ___/ ___/ ___|  | \\ | | __ _| | _____  __| | |  _ \\  __ _ _   _
+ | |   \\___ \\___ \\  |  \\| |/ _\` | |/ / _ \\/ _\` | | | | |/ _\` | | | |
+ | |___ ___) |__) | | |\\  | (_| |   <  __/ (_| | | |_| | (_| | |_| |
+  \\____|____/____/  |_| \\_|\\__,_|_|\\_\\___|\\__,_| |____/ \\__,_|\\__, |
+                                                              |___/`)
+    console.info("https://css-naked-day.github.io")
+  }
 })
 </script>
