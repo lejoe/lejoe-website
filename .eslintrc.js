9ed7f68f338ba93e4fbedf5c6ce62959ffb0bd18
lejoe <ciao@lejoe.com>
2023-04-11 22:41:45 +0200

    Re-adds CSS after CSS naked day

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,9 +11,7 @@ module.exports = {
   ],
   plugins: [],
   // add your custom rules here
-  rules: {
-    'no-console': 'off',
-  },
+  rules: {},
 
   overrides: [
     {
--- pages/index.vue
+++ pages/index.vue
@@ -14,18 +14,5 @@ import Vue from 'vue'
 
 export default Vue.extend({
   name: 'IndexPage',
-  mounted() {
-    // eslint-disable-next-line no-console
-    console.info("Hi! Wondering why there is no styles?")
-    console.info("Well… it's…")
-    console.info(`%c
-   ____ ____ ____    _   _       _            _   ____
-  / ___/ ___/ ___|  | \\ | | __ _| | _____  __| | |  _ \\  __ _ _   _
- | |   \\___ \\___ \\  |  \\| |/ _\` | |/ / _ \\/ _\` | | | | |/ _\` | | | |
- | |___ ___) |__) | | |\\  | (_| |   <  __/ (_| | | |_| | (_| | |_| |
-  \\____|____/____/  |_| \\_|\\__,_|_|\\_\\___|\\__,_| |____/ \\__,_|\\__, |
-                                                              |___/`, "font-family:monospace")
-    console.info("https://css-naked-day.github.io")
-  }
 })
 </script>
